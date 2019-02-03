96d7082a5131c985d98ea1e553e861ebf1d516f7
jonah <jonah.n.rubin@gmail.com>
2019-02-03 15:26:10 -0500

    Sequence_pair class integrated

--- main.cpp
+++ main.cpp
@@ -20,23 +20,16 @@ int main() {
     cout << "Enter the second sequence: ";
     getline(cin, strand2);
 
-    // initliaze grid with given strands and penalties
-    Grid grid = Grid(strand1, strand2, -2, -1, 1);
+    Sequence_pair seq_pair = Sequence_pair(strand1, strand2);
 
-    // print best global alignmemnts
-    cout << grid.get_aligned1() << endl;
-    cout << grid.get_aligned2() << endl;
+    // initliaze grid with given seuqence pair and penalties
+    Grid grid = Grid(seq_pair, -2, -1, 1);
+
+    // pair with best alignments
+    cout << seq_pair << endl;
 
     // formatted grid output
-//    cout << grid << endl;
-
-//    Sequence_pair seq_pair = Sequence_pair(strand1, strand2);
-
-//    cout << seq_pair << endl;
-
-
-
-
+    cout << grid << endl;
 
     return 0;
 }
